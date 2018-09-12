e1d3b5e6f8d1e2b61803bc30f431795f5fa99b46
svkote <36902132+svkote@users.noreply.github.com>
2018-09-13 00:55:13 +0300

    add php, first page

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
+<?php
+echo "Test";
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Php-Learn</title>
 </head>
 <body>
-    <h1></h1>
-
-
+    
 </body>
 </html>
