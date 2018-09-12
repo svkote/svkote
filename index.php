b6a63def40093139a0875181ddf9920c645e2d63
svkote <36902132+svkote@users.noreply.github.com>
2018-09-13 01:53:29 +0300

    table

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<?php
-echo "Test";
 
-
-
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,8 +7,25 @@ echo "Test";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Php-Learn</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+<h2>Вывод таблицы с помощью цикла while</h2>
+<?php
+$i = 1;
+echo "<table>";
+while ($i <= 10) {
+    echo "<tr>";
+    $n = 1;
+    while ($n <= 4) {
+        echo "<td> row $i | col $n </td>";
+        $n++;
+    }
+    echo "</tr>";
+    $i++;
+}
+echo "</table>";
+?>
+
 </body>
 </html>
